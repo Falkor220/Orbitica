3e49d7fb978c5b7a68fb1b194ba86027861c2958
Falkor220 <63330954+Falkor220@users.noreply.github.com>
2020-05-02 10:43:10 +0100

    start of infuser

---
+++ src/main/java/com/falkor220/orbitica/objects/blocks/Infuser.java
@@ -0,0 +1,10 @@
+package com.falkor220.orbitica.objects.blocks;
+
+import net.minecraft.block.Block;
+
+public class Infuser extends Block {
+    public Infuser(Properties properties) {
+        super(properties);
+    }
+}
+
